7ac514494e2cce9f0d694a853240c7890db5239d
Camilo Antonio Fava <camilo.fava@gmail.com>
2021-09-22 23:30:12 -0300

    Se define funcion que lee el problema y devuelve 2 diccionarios con las incompatibilidades y los tiempos. Se define una funcion que recibe estos diccionarios y genera un array de lavados. Por ahora da la solucion trivial menos optima, una prenda por lavado. Proximmente hacer una funcion que calcule el tiempo total de lavado, y optimizar funcion que genera la solucion.

---
+++ resolvedor.py
@@ -0,0 +1,58 @@
+def lector_problema():
+    my_file = open("primer_problema.txt", "r")
+    content = my_file.read()
+    content_list = content.split("\n")
+    
+    incopatibilidades = []
+    tiempos = {}
+    for e in content_list:
+        elemento =  e.split(" ")
+        
+        if(elemento[0] == 'e'):
+            incopatibilidades.append([int(elemento[1]), int(elemento[2])])
+
+        if(elemento[0] == 'n'):
+            tiempos[int(elemento[1])] = int(elemento[2])
+
+    incopatibilidades_ = {}
+
+    for prenda in tiempos.keys():
+        incopatibilidadesPrenda = []
+        for i in incopatibilidades:
+            if prenda in i:
+
+                prendaIncopatible = i[0] if i[0] !=prenda else i[1]
+
+                if(prendaIncopatible not in incopatibilidadesPrenda):
+                    incopatibilidadesPrenda.append(prendaIncopatible)
+
+
+        incopatibilidades_[prenda] = incopatibilidadesPrenda
+        
+
+    return incopatibilidades_, tiempos
+
+def analizador_problema():
+    incopatibilidades, tiempos = lector_problema()
+    generador_solucion(incopatibilidades, tiempos)
+
+def generador_solucion(incopatibilidades, tiempos):
+    lavados = []
+
+
+    for prenda in tiempos.keys():
+        lavadoPrenda= None
+        for lavado in lavados:
+            if prenda in lavado:
+                lavadoPrenda = lavado
+        if not lavadoPrenda:
+            lavados.append([prenda])
+            continue
+    
+    print(lavados)
+        
+            
+        
+    
+
+analizador_problema()
